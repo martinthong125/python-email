a0a95525d207ae1f34dc8ee898f4baa01c942696
Martin Thong <ventin75@gmail.com>
2022-02-01 17:32:41 +0800

    apply formating

--- emailclient.py
+++ emailclient.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''
+"""
 Email Client by Martin Thong, version 1.3
 Features:
 1. Hide user email and password. Stored under environment variables
@@ -9,24 +9,34 @@ Features:
 5. Preview before sending email
 6. Option to cancel sending email
 7. Login success message or catch login credentials error.
-'''
+"""
 
 import os
 import smtplib
 
+
 def get_email():
-    '''
+    """
     This function will ensure that the user key in a valid email address.
-    '''
+
+    Returns:
+        str: recipient email address
+    """
+
     email = input("Enter the recipent email address: ")
     while email.count("@") != 1 or email.count(".") < 1 or email.count(" ") > 0:
         email = input("Enter a valid recipent email address: ")
     return email
 
+
 def get_subject():
-    '''
+    """
     This function allows the user to key in the subject header till satisfied.
-    '''
+
+    Returns:
+        str: subject text
+    """
+
     subject = input("Type your subject header: ")
     choice = input("Do you want to change your subject header? (Y/N): ")
     while choice.upper() == "Y":
@@ -34,10 +44,15 @@ def get_subject():
         choice = input("Do you want to change your subject header? (Y/N): ")
     return subject
 
+
 def get_body():
-    '''
+    """
     This function allows the user to key in the body text till satisfied.
-    '''
+
+    Returns:
+        str: body text
+    """
+
     body = input("Type your body text: ")
     choice = input("Do you want to change your body text? (Y/N): ")
     while choice.upper() == "Y":
@@ -45,10 +60,17 @@ def get_body():
         choice = input("Do you want to change your body text? (Y/N): ")
     return body
 
+
 def print_email(email, subject, body):
-    '''
+    """
     This function will print a summary of how the email looks like.
-    '''
+
+    Args:
+        email (str): recipient email address
+        subject (str): subject text
+        body (str): body text
+    """
+
     print("*" * 40)
     print(f"From: {EMAIL_ADDRESS}")
     print(f"To: {email}")
@@ -56,25 +78,34 @@ def print_email(email, subject, body):
     print(f"Body: {body}")
     print("*" * 40)
 
+
 def confirm_send(email, subject, body, EMAIL_ADDRESS):
-    '''
+    """
     This function allows the user to finalise the email before sending out.
-    '''
+
+    Args:
+        email (str): recipient email address
+        subject (str): subject text
+        body (str): body text
+        EMAIL_ADDRESS (str): user email address
+    """
+
     confirm = input("Confirm send email? (Y/N): ")
     if confirm.upper() == "Y":
-        msg = f'Subject: {subject}\n\n{body}'
-        #print(msg)
+        msg = f"Subject: {subject}\n\n{body}"
+        # print(msg)
         smtp.sendmail(EMAIL_ADDRESS, email, msg)
         print(f"Email sent to: {email}")
     else:
         print("You have chosen not to send the email.")
+
 
 # Script starts here
 
 # get sensitive data from environment variables
 # https://www.youtube.com/watch?v=IolxqkL7cD8&t=0s
 EMAIL_ADDRESS = os.environ.get("EMAIL_ID")
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASS") 
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASS")
 # print(EMAIL_ADDRESS)
 # print(EMAIL_PASSWORD)
 
@@ -83,7 +114,7 @@ try:
     # https://www.youtube.com/watch?v=JRCJ6RtE3xU&t=590s
     # with smtplib.SMTP('localhost', 1025) as smtp:
     # python -m smtpd -c DebuggingServer -n localhost:1025
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
 
         # Check login credentials
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
@@ -94,7 +125,7 @@ try:
         subject = get_subject()
         body = get_body()
         print_email(email, subject, body)
-        
+
         # Confirm sending email?
         confirm_send(email, subject, body, EMAIL_ADDRESS)
 
